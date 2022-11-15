fbf6679b26aaf1265a32e27ea8f7ad58a742c54b
pavilonletoan <pavilion@charlotte.com>
2022-11-15 21:51:49 +0700

    .

--- App/database/Database.php
+++ App/database/Database.php
@@ -1,6 +1,6 @@
 <?php
     class Database{
-        const DB_DSN='mysql:host=localhost;port=3306;dbname=website_sammishop;charset=utf8';
+        const DB_DSN='mysql: host=localhost;port=3306;dbname=website_sammishop;charset=utf8';
         const DB_USERNAME='letoan';
         const DB_PASSWORD='letoan12320@E';
     }
